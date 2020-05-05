4840c14515489bca91e923c8fdf7da545d0e5e1b
ParkByungGeon <zzqudrjs@ajou.ac.kr>
2020-05-05 16:20:13 +0900

    The Third test

--- BowlingGame/src/Game.java
+++ BowlingGame/src/Game.java
@@ -1,14 +1,17 @@
 
 public class Game {
 
-	private int score=0;
+	private int[] rolls = new int[21];
+	private int crrentRoll = 0;
 
 	public void roll(int pins) {
-		
-		score += pins;
+		rolls [crrentRoll ++] = pins;
 	}
 
 	public int score() {
+		int score = 0;
+		for(int i =0; i<rolls.length; i++)
+			score += rolls[i];
 		return score;
 	}
 
